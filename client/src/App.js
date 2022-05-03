53fe8ecd648a7acd998bba47225873ea29caca18
Philipp Erker <Philipp.erker@protonmail.com>
2022-05-03 20:10:10 +0200

    add: folders pages and file NFTview.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import CardSlide from './components/CardSlide/CardSlide';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>test</h1>
       </header>
+      <main>
+        <p>NTF collection lorem....</p>
+      <CardSlide/>
+      {/* <p>NTF collection lorem....</p>
+      <CardSlide/>
+      <p>NTF collection lorem....</p>
+      <CardSlide/> */}
+      </main>
+     
+      
     </div>
   );
 }
