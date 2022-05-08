20564c706665db163e427cbaeb84ea81e3855cc4
Philipp Erker <Philipp.erker@protonmail.com>
2022-05-08 10:34:31 +0200

    update NFTview.spec.js

--- client/src/pages/NFTview.spec.js
+++ client/src/pages/NFTview.spec.js
@@ -2,26 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import NFTview from './NFTview';
 
-it('renders App.js', () => {
-  render(<NFTview />);
-  const mainElement = screen.getByRole('main');
-  expect(mainElement).toBeInTheDocument();
-});
-
-it('renders App.js', () => {
-  render(<NFTview />);
-  const collectionName1 = screen.getByTestId('collectionName_id_1');
-  expect(collectionName1).toBeInTheDocument();
-});
-
-it('renders App.js', () => {
-  render(<NFTview />);
-  const collectionName2 = screen.getByTestId('collectionName_id_2');
-  expect(collectionName2).toBeInTheDocument();
-});
-
-it('renders App.js', () => {
-  render(<NFTview />);
-  const collectionName3 = screen.getByTestId('collectionName_id_3');
-  expect(collectionName3).toBeInTheDocument();
+describe('render NTFview and text content', () => {
+  it('render NTFview and text content', () => {
+    render(<NFTview />);
+    const collectionName1 = screen.getByTestId('collectionName_id_1');
+    const collectionName2 = screen.getByTestId('collectionName_id_2');
+    const collectionName3 = screen.getByTestId('collectionName_id_3');
+    expect(collectionName1.textContent).toBe('NFT Collection: lorem....');
+    expect(collectionName2.textContent).toBe('NFT Collection: lorem....');
+    expect(collectionName3.textContent).toBe('NFT Collection: lorem....');
+  });
 });
