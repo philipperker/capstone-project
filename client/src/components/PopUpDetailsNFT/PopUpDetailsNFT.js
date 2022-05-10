0d5cf2a74e1c1fd4dc63bf7dda0ae186430d42a4
Philipp Erker <Philipp.erker@protonmail.com>
2022-05-10 13:41:12 +0200

    add PopUpDetailsNFT.js

---
+++ client/src/components/PopUpDetailsNFT/PopUpDetailsNFT.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function PopUpDetailsNFT({
+  open,
+  nftTokenId,
+  nftImageUrl,
+  nftSmartContact,
+}) {
+  console.log(nftTokenId);
+  console.log(nftImageUrl);
+
+  if (!open) return null;
+
+  return (
+    <PopUp>
+      <Image src={nftImageUrl}></Image>
+      <Content>Token-ID: {nftTokenId}</Content>
+      <Content>Smart Contact: </Content>
+      <Content>{nftSmartContact}</Content>
+    </PopUp>
+  );
+}
+
+const PopUp = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 90vw;
+  max-width: 90vw;
+  min-height: 70vh;
+  z-index: 1000;
+  position: fixed;
+  top: 20px;
+  background: red;
+  border: black;
+  border-radius: 30px;
+`;
+
+const Content = styled.div`
+  color: white;
+  margin-left: 3rem;
+`;
+
+const Image = styled.img`
+  margin-left: 3rem;
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+  width: 200px;
+  height: 200px;
+  border-radius: 20px;
+`;
