a174de89dcf193dce8709a3b79d3104ba2dd1f48
Philipp Erker <Philipp.erker@protonmail.com>
2022-05-10 11:41:40 +0200

    create Navigation.js

---
+++ client/src/components/Navigation/Navigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Navigation() {
+  return <Navbar></Navbar>;
+}
+
+const Navbar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+  background-color: red;
+  width: 100vw;
+  height: 100px;
+  bottom: 0;
+  position: fixed;
+`;
+
+const Button = styled.button`
+  border-radius: 50%;
+  margin: 0.5rem;
+  width: 80px;
+  height: 80px;
+`;
