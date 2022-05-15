8de79aae8b984c44835188f8816c921c7209bc68
Philipp Erker <Philipp.erker@protonmail.com>
2022-05-15 11:14:39 +0200

    install react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import NFTview from './pages/NFTview';
 import NFTadd from './pages/NFTadd/NFTadd';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <NFTview />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<NFTview />} />
+        <Route path="/add" element={<NFTadd />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
